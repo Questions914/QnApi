a8004e0891eceadca835de2a4853874b1acf3030
Michael Balloni <balloni.michael.sydney@outlook.com>
2024-01-05 20:46:56 -0800

    Deleting dist stuff, will be .gitignore'd from here on

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Auth, QnaAuth, AuthClass } from "./Auth.js";
-import { Connections, QnaConnections, ConnectionsClass } from "./Connections.js";
-import { QnA, QnAClass } from "./QnA.js";
-import { Users, QnaUsers, UsersClass } from "./Users.js";
-import { Utils, QnaUtils, UtilsClass, Handlers, QnaException, ench, encu } from "./Utils.js";
-import { QnaVideoRecorder } from "./VideoRecorder.js";
-export { Utils, QnaUtils, UtilsClass, Handlers, QnaException, ench, encu, Auth, QnaAuth, AuthClass, Users, QnaUsers, UsersClass, Connections, QnaConnections, ConnectionsClass, QnA, QnAClass, QnaVideoRecorder, };
-//# sourceMappingURL=index.d.ts.map
